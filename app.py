1e39c27897e20b75ac352e52917bf3afc06ddba7
rehan <rehan0edu123@gmail.com>
2026-04-11 23:43:57 +0530

    2nd

--- app.py
+++ app.py
@@ -3,10 +3,41 @@ import math
 import pandas as pd
 import matplotlib.pyplot as plt
 import csv
-import sys
 
-sys.path.append("c:/Users/Rehan/Desktop/python/terminal_velocity")
-from calulate_terminal_velocity import get_terminal_velocity
+def get_terminal_velocity(radius,density_of_particle,density_of_fluid,viscosity):
+    weight = (4*math.pi*(radius**3)*9.81*density_of_particle)/3
+    buoyancy = (4*math.pi*(radius**3)*9.81*density_of_fluid)/3
+    net_force = weight - buoyancy
+    velocity1 = (2*(radius**2)*(density_of_particle-density_of_fluid)*9.81)/(9*viscosity)
+    v_high =velocity1*2
+    v_low = 0
+    itteration = 0
+    while True:
+        v_mid = (v_high+v_low)/2
+        reynolds = (density_of_fluid*2*radius*v_mid)/viscosity
+        cd = (24/reynolds)*(1+0.15*reynolds**0.687)
+        drag_force = 0.5*cd*density_of_fluid*(math.pi)*(radius**2)*(v_mid**2)
+        
+        error = abs(drag_force-net_force)/net_force
+
+        if error <= 0.00005:
+            itteration+=1
+            if reynolds<1:
+                region = "Stokes region"
+            elif reynolds>1000:
+                region = "Newtons region"
+            else:
+                region = "Intermediate region"
+            return v_mid,drag_force,cd,reynolds,region,itteration,error
+            break
+
+        if drag_force>net_force:
+            itteration+=1
+            v_high=v_mid
+        elif drag_force<net_force:
+            itteration+=1
+            v_low=v_mid
+
 
 st.title("Terminal Velocity Calculator")
 
@@ -40,7 +71,7 @@ if st.button("Calculate"):
                                           "Terminal velocity(m/s)", "Drag(N)", "cd",
                                           "Reynolds", "Region", "iterations", "error"])
 
-        df.to_csv("c:/Users/Rehan/Desktop/python/terminal_velocity/results.csv", index=False)
+        df.to_csv("results.csv", index=False)
         st.success(f"Calculated {len(df)} combinations!")
         st.session_state.df = df
 
